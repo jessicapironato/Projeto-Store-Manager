c767ed38a06a4a9758d9bab9f4ba7c09f500f8c3
Jéssica <jessicapmaximo@gmail.com>
2023-05-26 18:30:50 -0300

    requisito 1 ok

---
+++ backend/tests/unit/services/productsService.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsService = require('../../../src/services/productsService');
+const productsModel = require('../../../src/models/productsModels');
+const { mockGetAll } = require('../mocks/productsModelMock');
+
+describe('Testa Camada Service', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Testa getAll', async function () {
+    sinon.stub(productsModel, 'getById').resolves(mockGetAll);
+
+    const result = await productsService.getAll();
+ 
+    expect(result).to.be.deep.equal(mockGetAll);
+  });
+
+  it('Testa getById existente', async function () {
+    sinon.stub(productsModel, 'getById').resolves(mockGetAll[0]);
+    const result = await productsService.getById(1);
+
+    expect(result).to.be.deep.equal(mockGetAll[0]);
+  });
+
+  it('Testa getById inexistente', async function () {
+    sinon.stub(productsModel, 'getById').resolves(undefined);
+
+    const result = await productsService.getById(30000);
+
+    expect(result).to.be.deep.equal(false);
+  });
+});
