408e47f5138d2b4cff53d6a91bba4325a7e286e0
Jéssica <jessicapmaximo@gmail.com>
2023-05-26 17:29:20 -0300

    testes camada model ok

---
+++ backend/tests/unit/mocks/productsModelMock.js
@@ -0,0 +1,24 @@
+const mockGetAll = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+  {
+    id: 3,
+    name: 'Escudo do Capitão América',
+  },
+];
+
+const mockById = {
+  id: 1,
+  name: 'Martelo de Thor',
+};
+
+module.exports = {
+  mockGetAll,
+  mockById,
+};
---
+++ backend/tests/unit/models/productsModels.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsModel = require('../../../src/models/productsModels');
+const connection = require('../../../src/models/connection');
+const { mockGetAll } = require('../mocks/productsModelMock');
+
+describe('Testa a camada Model', function () {
+  afterEach(sinon.restore);
+  
+  it('Testando getAll', async function () {
+    sinon.stub(connection, 'execute').resolves([mockGetAll]);
+
+    const result = await productsModel.getAll();
+
+    expect(result).to.be.deep.equal(mockGetAll);
+  });
+
+  it('Testando getById', async function () {
+    sinon.stub(connection, 'execute').resolves([[mockGetAll[0]]]);
+
+    const result = await productsModel.getById(0);
+
+    expect(result).to.be.deep.equal(mockGetAll[0]);
+  });
+});
