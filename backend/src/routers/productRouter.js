01ef511d3d29640d9b276847abadb5706c4e6b13
Jéssica <jessicapmaximo@gmail.com>
2023-05-28 21:29:59 -0300

    requisito 4 ok - faltam testes 3 e 4

--- backend/src/routers/productRouter.js
+++ backend/src/routers/productRouter.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const productControler = require('../controllers/productsControler');
+const nameIsValid = require('../middlewares/validateProducts');
 
 router.get('/', productControler.getAll);
 router.get('/:id', productControler.getById);
-router.post('/', productControler.create);
+router.post('/', nameIsValid, productControler.create);
 
 module.exports = router;
